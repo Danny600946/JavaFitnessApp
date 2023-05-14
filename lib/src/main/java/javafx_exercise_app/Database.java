940c4bcf886a3440e54686ca3a923d9fb56c108b
Daniel Vousden <d.vousden600946@gmail.com>
2023-05-15 00:40:00 +0100

    Added a method to insert data into the db. Used this method in SignUpUI. Also added more comments to these classes

--- lib/src/main/java/javafx_exercise_app/Database.java
+++ lib/src/main/java/javafx_exercise_app/Database.java
@@ -2,44 +2,59 @@ package javafx_exercise_app;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Database {
-	
-	private static Connection conn = null; 
 
-	
-    public static void connect() {  
-	   
-	     try {  
-	         // db parameters  
-	         String url = "jdbc:sqlite:C:\\sqlite\\GetFit_db.db";  
-	         // create a connection to the database  
-	         conn = DriverManager.getConnection(url);  
-	              
-	         System.out.println("Connection to SQLite has been established.");  
-	              
-	     } catch (SQLException e) {  
-	         System.out.println(e.getMessage());  
-	     } 
-	 } 
-	
-	public static void closeConnection() {
-		
-		try {  
-            if (conn != null) {  
-                conn.close();  
-                System.out.println("Connection Terminated");
-            }  
-        } catch (SQLException ex) {  
-            System.out.println(ex.getMessage()); 
-		
+	private static Connection conn = null;
+
+	public static void connect() {
+
+		try {
+			// db parameters
+			String url = "jdbc:sqlite:C:\\sqlite\\GetFit_db.db";
+			// create a connection to the database
+			conn = DriverManager.getConnection(url);
+
+			System.out.println("Connection to SQLite has been established.");
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
-}
-	
-	public void insertNewUser(String username, String password, String email) {}
-}
+	public static void closeConnection() {
 
+		try {
+			if (conn != null) {
+				conn.close();
+				System.out.println("Connection Terminated");
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+
+		}
+
+	}
+
+	public static void insertNewUser(RegistrationInfo userDetails) throws SQLException {
+		String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+		connect();
+		PreparedStatement statement = conn.prepareStatement(sql);
+		statement.setString(1, userDetails.getUsername());
+		statement.setString(2, userDetails.getPassword());
+		statement.setString(3, userDetails.getEmail());
+
+		try {
+			statement.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			statement.close();
+			closeConnection();
+		}
+	}
+}
--- lib/src/main/java/javafx_exercise_app/SignUpUI.java
+++ lib/src/main/java/javafx_exercise_app/SignUpUI.java
@@ -1,5 +1,7 @@
 package javafx_exercise_app;
 
+import java.sql.SQLException;
+
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -16,6 +18,7 @@ public class SignUpUI extends StartUI {
 	private static TextField emailTextField = new TextField();
 
 	public SignUpUI() {
+		BorderPane.setAlignment(returnToLoginPageButton, Pos.CENTER);
 		addListeners();
 		setClasses();
 		setIds();
@@ -27,7 +30,6 @@ public class SignUpUI extends StartUI {
 	 * @return returnToLoginPageButton The button to return to the login page
 	 */
 	public Button getCenteredReturnButton() {
-		BorderPane.setAlignment(returnToLoginPageButton, Pos.CENTER);
 		return returnToLoginPageButton;
 	}
 
@@ -55,30 +57,47 @@ public class SignUpUI extends StartUI {
 	 * Adds appropriate listeners to each node.
 	 */
 	private void addListeners() {
+		// Listener for the login button on the registration page.
 		returnToLoginPageButton.setOnAction(event -> {
+			// Changes the navBar button to sign up.
 			navBar.getChildren().remove(returnToLoginPageButton);
 			navBar.getChildren().add(LoginUI.getSignUpButton());
+			// Changes the inputPane to the login format.
 			LoginUI.formatInputPane();
 		});
-
+		// Listener for the register button on the registration page.
 		registerButton.setOnAction(event -> {
+			// Creates a new registration object with the inputs.
 			RegistrationInfo newUser = new RegistrationInfo(usernameTextField.getText(), passwordField.getText(),
 					emailTextField.getText());
-
+			// If all inputs are valid then the registration is accepted.
 			if (newUser.validateUsername().size() == 0 & newUser.validatePassword().size() == 0
 					& newUser.validateEmail().size() == 0) {
+				// Re-formats the input pane.
 				formatInputPane();
+				// Adds a confirmation message below register button.
 				inputPane.add(centerGridPaneNode(new Label("Registration Successful")), 0, 12);
+				// Still needs to send to db.
+				try {
+					Database.insertNewUser(newUser);
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
 			}
-
+			/*
+			 * If the inputs are not valid then the validation messages are displayed for
+			 * the user to see below their respective labels. For example the username
+			 * validation messages will appear below the username text field.
+			 */
 			else {
+				// Empties the input pane.
 				inputPane.getChildren().clear();
 				inputPane.add(centerGridPaneNode(registerLabel), 0, 0);
 				inputPane.add(new Label(), 0, 1);
 				inputPane.add(centerGridPaneNode(usernameLabel), 0, 2);
 				inputPane.add(centerGridPaneNode(usernameTextField), 0, 3);
 				usernameTextField.setPromptText("Enter Username");
-
+				// Loops through each username validation message and adds it to the input pane.
 				int i = 0;
 				while (i < newUser.getUsernameMessageList().size()) {
 					inputPane.add(centerGridPaneNode(new Label(newUser.getUsernameMessageList().get(i))), 0, 4 + i);
@@ -89,7 +108,7 @@ public class SignUpUI extends StartUI {
 				inputPane.add(centerGridPaneNode(passwordLabel), 0, 5 + i);
 				inputPane.add(centerGridPaneNode(passwordField), 0, 6 + i);
 				passwordField.setPromptText("Enter Password");
-
+				// Loops through each password validation message and adds it to the input pane.
 				int j = 0;
 				while (j < newUser.getPasswordMessageList().size()) {
 					inputPane.add(centerGridPaneNode(new Label(newUser.getPasswordMessageList().get(j))), 0, 7 + i + j);
@@ -99,7 +118,7 @@ public class SignUpUI extends StartUI {
 				inputPane.add(centerGridPaneNode(emailLabel), 0, 8 + i + j);
 				inputPane.add(centerGridPaneNode(emailTextField), 0, 9 + i + j);
 				emailTextField.setPromptText("Enter E-mail");
-
+				// Loops through each email validation message and adds it to the input pane.
 				int h = 0;
 				while (h < newUser.getEmailMessageList().size()) {
 					inputPane.add(centerGridPaneNode(new Label(newUser.getEmailMessageList().get(h))), 0,
@@ -124,23 +143,27 @@ public class SignUpUI extends StartUI {
 		usernameTextField.clear();
 		passwordField.clear();
 		emailTextField.clear();
-
+		// Adds login Label
 		inputPane.add(centerGridPaneNode(registerLabel), 0, 0);
 		inputPane.add(new Label(), 0, 1);
+		// Adds username Label and TextField.
 		inputPane.add(centerGridPaneNode(usernameLabel), 0, 2);
 		inputPane.add(centerGridPaneNode(usernameTextField), 0, 3);
 		usernameTextField.setPromptText("Enter Username");
 		inputPane.add(new Label(), 0, 4);
+		// Adds password Label and PasswordField.
 		inputPane.add(centerGridPaneNode(passwordLabel), 0, 5);
 		inputPane.add(centerGridPaneNode(passwordField), 0, 6);
 		passwordField.setPromptText("Enter Password");
 		inputPane.add(new Label(), 0, 7);
+		// Adds email Label and TextField.
 		inputPane.add(centerGridPaneNode(emailLabel), 0, 8);
 		inputPane.add(centerGridPaneNode(emailTextField), 0, 9);
 		emailTextField.setPromptText("Enter E-mail");
 		inputPane.add(new Label(), 0, 10);
+		// Adds registration button.
 		inputPane.add(centerGridPaneNode(registerButton), 0, 11);
-
+		// Centres the GridPane.
 		inputPane.setAlignment(Pos.CENTER);
 	}
 
